44ffc00e8178bcd3af7a3aa318c44cfaea90091d
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-04-30 18:16:19 +0700

    Update Dikit Set Up Shooter

--- This is Blast! New Test/Assets/Script/Shooter/ShooterActive.cs
+++ This is Blast! New Test/Assets/Script/Shooter/ShooterActive.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ShooterActive : MonoBehaviour
@@ -7,19 +8,25 @@ public class ShooterActive : MonoBehaviour
     [SerializeField] float fireRate;
     [SerializeField] int ammo;
     [SerializeField] GameObject bullet;
+    [SerializeField] TextMeshProUGUI ammoText;
     //[SerializeField] bool test; //Bisa di hapus nanti
 
     string targetCube => targetColor.ToString();
+    private void Awake()
+    {
+        ammoText = GetComponentInChildren<TextMeshProUGUI>();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        ammoText.text = ammo.ToString();
         //if (test)
         //{
         //    Debug.Log(gameObject.name + " Target " + targetCube);

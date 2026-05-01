855b96773849f815f77fdc4f5d3e3e03690c4afe
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-05-01 14:48:35 +0700

    Pindah ke Input Lama

--- This is Blast! New Test/Assets/Script/Shooter/ShooterActive.cs
+++ This is Blast! New Test/Assets/Script/Shooter/ShooterActive.cs
@@ -19,7 +19,7 @@ public class ShooterActive : MonoBehaviour
     string targetCube => targetColor.ToString();
     private void Awake()
     {
-        ammoText = GetComponentInChildren<TextMeshProUGUI>();
+        //ammoText = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -100,7 +100,7 @@ public class ShooterActive : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ammoText.text = ammo.ToString();
+        //ammoText.text = ammo.ToString();
         ShooterUpdateState();
 
         //if (test)

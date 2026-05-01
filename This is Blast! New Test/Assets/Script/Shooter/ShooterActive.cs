4c6ae587d512779d1708688dd923f0f195539ea1
SenHayato <157812043+SenHayato@users.noreply.github.com>
2026-05-01 14:27:08 +0700

    Update ShooterActive.cs

--- This is Blast! New Test/Assets/Script/Shooter/ShooterActive.cs
+++ This is Blast! New Test/Assets/Script/Shooter/ShooterActive.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 public class ShooterActive : MonoBehaviour
 {
-    [Header("Shooter Component")]
+    [Header("Target Cube")]
     [SerializeField] TargetType targetColor; //Same color as the shooter
+    [SerializeField] GameObject shootingTarget;
+
+    [Header("Shooter Component")]
+    [SerializeField] ShooterState shooterState;
+    [SerializeField] bool isInteractable;
     [SerializeField] float fireRate;
     [SerializeField] int ammo;
     [SerializeField] GameObject bullet;
@@ -20,17 +25,90 @@ public class ShooterActive : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+    }
+
+    #region ShooterState
+    void MovingForward()
+    {
+
+    }
+
+    void Stopping()
+    {
+
+    }
+
+    void MovingToPosition()
+    {
+
+    }
+
+    void StopInPosition()
+    {
+
+    }
+
+    void ShootingBullet()
+    {
+
+    }
+
+    void MovingOut()
+    {
+
+    }
+
+    #endregion
+
+    void ShooterUpdateState()
+    {
+        if (shooterState == ShooterState.Stop)
+        {
+            Stopping();
+        }
+        else if (shooterState == ShooterState.MoveForward)
+        {
+            MovingForward();
+        }
+        else if (shooterState == ShooterState.GoToPosition)
+        {
+            MovingToPosition();
+        }
+        else if (shooterState == ShooterState.InPosition)
+        {
+            StopInPosition();
+        }
+        else if (shooterState == ShooterState.Shooting)
+        {
+            ShootingBullet();
+        }
+        else if (shooterState == ShooterState.MovingOut)
+        {
+            MovingOut();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         ammoText.text = ammo.ToString();
+        ShooterUpdateState();
+
         //if (test)
         //{
         //    Debug.Log(gameObject.name + " Target " + targetCube);
         //}
+    }
+
+
+    enum ShooterState
+    {
+        //Masih di start
+        MoveForward, Stop,
+
+        //Setelah di klik
+        GoToPosition, InPosition, Shooting, MovingOut
     }
 }
 
